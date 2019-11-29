e74a6ff31fff7ac2b7a7deb9ef05de4362aa3c53
ThisisEmam <abdoemamofficial@gmail.com>
2019-11-29 07:25:07 +0200

    secondUpload

--- js/landingPage.js
+++ js/landingPage.js
@@ -1,27 +1,52 @@
 $(function () {
     "use strict";
+    var username;
+    var password;
+    var email;
+
+
+
     $("#btnLogin").click(function (event) {
         event.preventDefault();
-        var userName = $('#uname1').val();
-        var passWord = $('#pwd1').val();
+        username = $('#loginUsername').val();
+        password = $('#loginPassword').val();
         // userName == 'admin' && passWord == '123'
-        if (userName == 'admin' && passWord == '123') {
-            // $('#loggedIn').load('manager.html .managerMainWrapper');
-            // $('#main-wrapper').hide();
-            // $('#loginModal').modal("hide");
-            window.location.href='manager.html';
+        if (username == 'admin' && password == '123') {
+            window.location.href = 'manager.html';
         } else {
-            alert('Wrong!');
+            $('#loginModal').modal('hide');
+            $('#loginUsername').val("");
+            $('#loginPassword').val("");
+            setTimeout(function () {
+                $('.loginFailAlert').css("display", "block");
+            }, 500);
+            setTimeout(function () {
+                $('.loginFailAlert').css("display", "none");
+            }, 3500);            
         }
     });
 
-    
+    $("body").on('click','#btnSignUp',function (event) {
+
+        $('#signUpUsername').val("");
+        $('#signUpEmail').val("");
+        $('#signUpPassword').val("");
+        $('#signUpModal').modal('hide');
+
+        setTimeout(function () {
+            $('.signUpSuccessAlert').css("display", "block");
+        }, 500);
+        setTimeout(function () {
+            $('.signUpSuccessAlert').css("display", "none");
+        }, 3500);
+    });
+
+
 
     // $(document).ready(function () {
-
-    //     // $('#loggedIn').load('manager.html .managerMainWrapper');
-    //     // $('#main-wrapper').hide();
-    //     // $('#loginModal').modal("hide");
-    //     // window.location.href='manager.html';
+    // $('#loggedIn').load('manager.html .managerMainWrapper');
+    // $('#main-wrapper').hide();
+    // $('#loginModal').modal("hide");
+    // window.location.href='manager.html';
     // });
 });
